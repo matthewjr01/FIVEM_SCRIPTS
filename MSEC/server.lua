a2b63e7a35b2aa6fd6a028ba787532e7d905be7b
matthewjr01 <68038796+matthewjr01@users.noreply.github.com>
2021-11-03 10:48:33 -0700

    UPDATE

--- MSEC/server.lua
+++ MSEC/server.lua
@@ -4,7 +4,6 @@ AddEventHandler('playerConnecting', function(name, setReason)
     local source = source
     Citizen.Wait(300)
     local Identifiers = GetPlayerIdentifier(source)
-    print("PID:"+ GET_PLAYER_SERVER_ID(name))
     print("GOT NEW PLAYER WITH ID: "+ identifier)
     MySQL.Async.fetchAll("SELECT * FROM `usi` WHERE `SteamHex` LIKE @SteamHex",{['@SteamHex'] = identifier}, 
     function(result)
